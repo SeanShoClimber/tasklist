7c890ce51ff8d3e194db002be35a647065d82181
Sean.Shin <gitboulder@gmail.com>
2024-07-14 19:11:07 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,7 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
-import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,36 +10,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.TasksDTO;
-import utils.DBUtil;
-
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
+
     }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
 
-        List<TasksDTO> tasks = em.createNamedQuery("getAllTasks",TasksDTO.class).getResultList();
+       request.setAttribute("_token", request.getSession().getId());
 
-        em.close();
+       request.setAttribute("TasksDTO", new TasksDTO());
 
-        request.setAttribute("tasks", tasks);
-
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/indexfortask.jsp");
-        rd.forward(request, response);
+       RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/newfortask.jsp");
+       rd.forward(request, response);
     }
 
 }
